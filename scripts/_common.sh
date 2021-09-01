043832f1fc92842d17e8a21ec35d7a49d27e27cc
siwinter <45730097+siwinter@users.noreply.github.com>
2021-09-01 13:24:45 +0200

    Update _common.sh

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -5,9 +5,11 @@
 #=================================================
 
 # dependencies used by the app
-pkg_dependencies_x86="libavahi-client3 libavahi-common3 libc6 libdbus-1-3 libssl1.1 libstdc++6 liburiparser1 zlib1g dvb-apps bzip2"
-pkg_dependencies_rb="libavahi-client3 libavahi-common3 libc6 libdbus-1-3 libssl1.1 liburiparser1 zlib1g dvb-apps bzip2"
+#pkg_dependencies_x86="libavahi-client3 libavahi-common3 libc6 libdbus-1-3 libssl1.1 libstdc++6 liburiparser1 zlib1g dvb-apps bzip2"
+#pkg_dependencies_rb="libavahi-client3 libavahi-common3 libc6 libdbus-1-3 libssl1.1 liburiparser1 zlib1g dvb-apps bzip2"
 
 # deb package URLs
 tvheadend_deb_x86_64="../sources/tvheadend_4.2.8-dmo1_amd64.deb"
 tvheadend_deb_arm="../sources/tvheadend_4.2.8-dmo1_armhf.deb.deb"
+tvheadend_data="tvheadend-data_4.2.8-dmo1_all.deb"
+dtv_scan_tables="dtv-scan-tables_0+git20190925.6d01903-0.1~deb10u1_all.deb"
