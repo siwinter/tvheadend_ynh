de24661285c5ef49165b9ff9d39aab534c77566e
siwinter <45730097+siwinter@users.noreply.github.com>
2021-09-19 11:32:47 +0200

    Update _common.sh

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -9,9 +9,3 @@
 pkg_dependencies_arm="libavahi-client3 libavahi-common3 libavcodec58 libavformat58 libavfilter7 libavformat58 \
                       libavresample4 libavutil56 libc6 libdbus-1-3 libdvbcsa1 libhdhomerun4 libssl1.1 liburiparser1 zlib1g  \
                       adduser lsb-base dvb-apps"
-
-# deb package URLs
-tvheadend_deb_x86_64="../sources/tvheadend_4.2.8-dmo1_amd64.deb"
-tvheadend_deb_arm="../sources/tvheadend_4.2.8-dmo1_armhf.deb.deb"
-tvheadend_data="tvheadend-data_4.2.8-dmo1_all.deb"
-dtv_scan_tables="dtv-scan-tables_0+git20190925.6d01903-0.1~deb10u1_all.deb"
