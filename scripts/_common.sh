54eb71edda0cde605e195a5c5887316d926d8bac
siwinter <45730097+siwinter@users.noreply.github.com>
2021-09-30 23:55:49 +0200

    Update _common.sh

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -5,12 +5,7 @@
 #=================================================
 
 # dependencies used by the app
-#pkg_dependencies_x86="libavahi-client3 libavahi-common3 libc6 libdbus-1-3 libssl1.1 libstdc++6 liburiparser1 zlib1g dvb-apps bzip2"
-#pkg_dependencies_arm="libavahi-client3 libavahi-common3 libavcodec58 libavformat58 libavfilter7 libavformat58 \
-#                      libavresample4 libavutil56 libc6 libdbus-1-3 libdvbcsa1 libhdhomerun4 libssl1.1 liburiparser1 zlib1g  \
-#                      adduser lsb-base dvb-apps"
 
-tvheadend_zip_url="https://github.com/siwinter/tvheadend_ynh/raw/master/sources/tvheadend_4.2.8-dmo1_armhf.zip"
 # set dependencies and url of tvhead due to architecture 
 if [ -n "$(uname -m | grep arm)" ]
     then
@@ -38,22 +33,20 @@ myynh_install() {
     ynh_script_progression --message="Installing dependencies..." --weight=44
     
     ynh_install_app_dependencies $pkg_dependencies
-  
-    #ynh_install_app_dependencies $pkg_dependencies
-    
+
     #=================================================
     # DOWNLOAD TVHEADEND PACKAGES
     #=================================================
-    #ynh_script_progression --m essage="Downloading Tvheadend..." --weight=5
+    ynh_script_progression --m essage="Downloading Tvheadend..." --weight=5
     
-    #temp_folder="$(mktemp -d)"
-    #tvheadend-data_dev_dst="$temp_folder/tvheadend-data.deb"
-    #dtv-scan-tables_deb_dst="$temp_folder/dtv-scan-tables.deb"
-    #tvheadend_zip_dest="$temp_folder/tvheadend.zip"
-    #ynh_exec_quiet "wget -q -O $tvheadend-data_deb_dst $tvheadend-data_deb_url"
-    #ynh_exec_quiet "wget -q -O $dtv-scan-tables_deb_dst $dtv-scan-tables_deb_url"
+    temp_folder="$(mktemp -d)"
+    tvheadend-data_dev_dst="$temp_folder/tvheadend-data.deb"
+    dtv-scan-tables_deb_dst="$temp_folder/dtv-scan-tables.deb"
+    tvheadend_zip_dest="$temp_folder/tvheadend.zip"
+    ynh_exec_quiet "wget -q -O $tvheadend-data_deb_dst $tvheadend-data_deb_url"
+    ynh_exec_quiet "wget -q -O $dtv-scan-tables_deb_dst $dtv-scan-tables_deb_url"
     
-    #ynh_exec_quiet "wget -q -O $tvheadend_zip_dst $tvheadend_zip_url"
+    ynh_exec_quiet "wget -q -O $tvheadend_zip_dst $tvheadend_zip_url"
     
     #=================================================
     # INSTALL TVHEADEND DEB PACKAGES
