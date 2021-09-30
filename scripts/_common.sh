056520c09e2bb710fc7b8b0a2eeaa7927b978aa1
siwinter <45730097+siwinter@users.noreply.github.com>
2021-09-30 19:46:23 +0200

    Update _common.sh

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -9,7 +9,7 @@
 
 # dependencies used by the app
 #pkg_dependencies_x86="libavahi-client3 libavahi-common3 libc6 libdbus-1-3 libssl1.1 libstdc++6 liburiparser1 zlib1g dvb-apps bzip2"
-pkg_dependencies="libavahi-client3 libavahi-common3 libavcodec58 libavformat58 libavfilter7 libavformat58 \
+pkg_dependencies_arm="libavahi-client3 libavahi-common3 libavcodec58 libavformat58 libavfilter7 libavformat58 \
                       libavresample4 libavutil56 libc6 libdbus-1-3 libdvbcsa1 libhdhomerun4 libssl1.1 liburiparser1 zlib1g  \
                       adduser lsb-base dvb-apps"
 
@@ -40,8 +40,8 @@ myynh_install() {
     #=================================================
     ynh_script_progression --message="Installing dependencies..." --weight=44
     
-    ynh_install_app_dependencies $pkg_dependencies
-    
+#    ynh_install_app_dependencies $pkg_dependencies
+    ynh_install_app_dependencies $pkg_dependencies_arm
     #=================================================
     # DOWNLOAD TVHEADEND PACKAGES
     #=================================================
