c8ae0e80e90de4123fbc95d95c68da8c79425058
siwinter <45730097+siwinter@users.noreply.github.com>
2021-09-30 23:30:43 +0200

    Update _common.sh

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -38,7 +38,6 @@ myynh_install() {
     ynh_script_progression --message="Installing dependencies..." --weight=44
  
  pkg_dependencies_arm="libavahi-client3 libavahi-common3 libavcodec58 libavformat58 libavfilter7 libavformat58 \
-                      libavresample4 libavutil56 libc6 libdbus-1-3 libdvbcsa1 libhdhomerun4 libssl1.1 liburiparser1 zlib1g  \
                       adduser lsb-base dvb-apps"
  
  
