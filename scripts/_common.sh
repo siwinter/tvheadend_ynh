7bbc8b4121dd85b8d2515711aa0472fd9c8c4dd4
siwinter <45730097+siwinter@users.noreply.github.com>
2021-10-01 11:01:21 +0200

    Update _common.sh

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -21,8 +21,8 @@ if [ -n "$(uname -m | grep arm)" ]
         tvheadend_zip_url="$sources_url/tvheadend_4.2.8-dmo1_amd64.zip"
 fi
 
-tvheadend-data_deb_url="$sources_url/tvheadend-data_4.2.8-dmo1_all.deb"
-dtv-scan-tables_deb_url="$sources_url/dtv-scan-tables_0%2Bgit20190925.6d01903-0.1%7Edeb10u1_all.deb"
+ynh_exec_quiet tvheadend-data_deb_url="$sources_url/tvheadend-data_4.2.8-dmo1_all.deb"
+ynh_exec_warn_less dtv-scan-tables_deb_url="$sources_url/dtv-scan-tables_0%2Bgit20190925.6d01903-0.1%7Edeb10u1_all.deb"
 
 
 myynh_install() {
