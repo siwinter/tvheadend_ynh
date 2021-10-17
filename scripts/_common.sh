a63dddb8d2f28958cb1f89e74f28d4880b54c3d5
siwinter <45730097+siwinter@users.noreply.github.com>
2021-10-17 22:56:31 +0200

    Update _common.sh

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -9,15 +9,16 @@ sources_url="https://github.com/siwinter/tvheadend_ynh/raw/master/sources"
 # set dependencies and url of tvhead due to architecture 
 if [ -n "$(uname -m | grep arm)" ]
     then
-        pkg_dependencies="libavahi-client3 libavahi-common3 libavcodec58 libavformat58 libavfilter7 libavformat58 \
-                      libavresample4 libavutil56 libc6 libdbus-1-3 libdvbcsa1 libhdhomerun4 libssl1.1 liburiparser1 zlib1g  \
-                      adduser lsb-base dvb-apps"
+        pkg_dependencies="libavahi-client3 libavahi-common3 libavcodec58 libavformat58 libavfilter7 libavresample4 \
+                      libavutil56 libc6 libdbus-1-3 libdvbcsa1 libhdhomerun4 libssl1.1 liburiparser1 zlib1g  \
+                      lsb-base dvb-apps"
                       
         tvheadend_zip_url="$sources_url/tvheadend_4.2.8-dmo1_armhf.zip"
     else
-        pkg_dependencies="libavahi-client3 libavahi-common3 libc6 libdbus-1-3 libssl1.1 libstdc++6 liburiparser1 zlib1g \
-                      dvb-apps bzip2"
-                      
+        pkg_dependencies="libavahi-client3 libavahi-common3 libavcodec58 libavfilter7 libavformat58 libavresample4 \
+                      libavutil56 libc6 libdbus-1-3 libdvbcsa1 libhdhomerun4 libssl1.1 liburiparser1 zlib1g \
+                      lsb-base dvb-apps bzip2"
+        
         tvheadend_zip_url="$sources_url/tvheadend_4.2.8-dmo1_amd64.zip"
 fi
 
